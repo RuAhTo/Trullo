5131a8d9c0d28315372a67b44cc5b3ba73532c3d
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-10-01 11:25:14 +0200

    Database init, dependencies, index.ts init

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express, {Request, Response, NextFunction} from "express";
+import dotenv from 'dotenv';
+import cors from 'cors'
+import { PrismaClient } from "@prisma/client";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}!`);
+})
+
+process.on('SIGNIT', async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+})
